8213e4c4128f978f5f511e9f0fa278d53c4605ad
OjanRN <ojanrn5@gmail.com>
2023-02-09 09:41:44 +0700

    randomrized blank points

--- matrixw.py
+++ matrixw.py
@@ -1,15 +1,24 @@
 ## By m7d9ng github.com/ojanrn
-import random, os, sys
+import random, os, time
 
 def matrix_get(char_count):
     wordlist = "abcdefghiojklmnopqrstuvwxyz1234567890"
     final_list = ""
-    for i in range(char_count):
+    for i in range(char_count - 1):
         final_list += random.choice(wordlist)
     return_list = final_list
     final_list = ""
     return return_list
-    
+
+def matrix_line(matrix_list):
+    print_list = []
+    for chars in matrix_list:
+        des_put = random.randint(0,1)
+        if des_put == 0:
+            print_list.append(chars)
+        else:
+            print_list.append(" ")
+    return print_list
 
 def term_settings():
     term_size = os.get_terminal_size()
@@ -20,6 +29,8 @@ if __name__ == "__main__":
     while True:
         current_term_width = term_settings()
         base_term = matrix_get(current_term_width)
-        print(base_term, end='')
+        print("".join(matrix_line(base_term)))
+        time.sleep(0.01)
+
 
 
